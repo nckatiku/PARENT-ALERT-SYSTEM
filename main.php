d74bafe0b60687eee7d8be45278c77d460d6f657
rajiv-shrivastava <rjv834@gmail.com>
2015-05-13 14:42:02 +0530

    swapnil add file

--- main.php
+++ main.php
@@ -27,40 +27,15 @@ header("Location:login_lit.php");
     $("#datepicker").datepicker();
   });
   </script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Dashboard">
-    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
-
-    <title>Liitle Soldiers School</title>
-
-    <!-- Bootstrap core CSS -->
-	<link rel="stylesheet" type="text/css" href="css/mycss.css" />
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-       
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="assets/custom.css"> 
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <!--external css-->
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-		
-    <link rel="stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
-    <link rel="stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
-    <link rel="stylesheet" type="text/css" href="assets/lineicons/style.css">    
     
-    <!-- Custom styles for this template -->
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/style-responsive.css" rel="stylesheet">
+ <?php
 
-    <script src="assets/js/chart-master/Chart.js"></script>
-    
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+    include 'layout.php';
+
+   ?>
+  
+
+       </head>
 <body>
 <section id="container" >
 	<!--header start-->
@@ -81,7 +56,7 @@ header("Location:login_lit.php");
             	<ul class="nav pull-right top-menu">
 
               <button class="btn btn-warning" type="button">
-                          <a href="logout.php"><span style="color:white"> Logout <i class="glyphicon glyphicon-backward"> </i></span></a>
+                          <a href="logout.php"><span style="color:white"> Logout <i class="glyphicon glyphicon-off"> </i></span></a>
                             </button>
             	</ul>
             </div>
@@ -104,7 +79,7 @@ header("Location:login_lit.php");
 				  <li >
                       <a href="myhome.php">
                           <i class="fa fa-dashboard"></i>
-                          <span>Back to home</span>
+                          <span> Home</span>
                       </a>
                   </li>
                   <li class="sub-menu active">
@@ -117,19 +92,13 @@ header("Location:login_lit.php");
 				      <li class="sub-menu active">
                       <a href="mycompose.php" >
                           <i class="fa fa-tasks"></i>
-                          <span>Select New Class</span>
+                          <span>Back To Compose</span>
                       </a>
                    </li>
 
 
 
-           <li>
-          <a class ="active" href="simple.php">
-                          <i class="fa fa-dashboard"></i>
-                          <span>New Event Message</span>
-                      </a>
-                  </li>
-
+         
            </ul>   
 
 
@@ -427,7 +396,7 @@ header("Location:login_lit.php");
 										    <div class="form-group">
                                            <label class="col-sm-2 col-sm-2 control-label"><h5>Where</h5></label>
                                          <div class="col-sm-10">
-                   <input type="text" class="form-control round-form" name ="where" value="Venue:Little Soldiers campus" required>
+                   <input type="text" class="form-control round-form" name ="where" value="Venue:Little Soldiers campus" />
                                          <span class="help-block">Venue/place of the event</span>
                                           </div>
                                              </div>
@@ -491,60 +460,11 @@ header("Location:login_lit.php");
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="js/bootstrap.js"></script>
 
-    <script src="js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="assets/js/jquery.scrollTo.min.js"></script>
-    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="assets/js/jquery.sparkline.js"></script>
+      
+  <?php
+  include 'layout2.php';
 
-
-    <!--common script for all pages-->
-    <script src="assets/js/common-scripts.js"></script>
-    
-    <script type="text/javascript" src="assets/js/gritter/js/jquery.gritter.js"></script>
-    <script type="text/javascript" src="assets/js/gritter-conf.js"></script>
-
-    <!--script for this page-->
-    <script src="assets/js/sparkline-chart.js"></script>    
-	<script src="assets/js/zabuto_calendar.js"></script>	
-	
-	
-	
-	<script type="application/javascript">
-        $(document).ready(function () {
-            $("#date-popover").popover({html: true, trigger: "manual"});
-            $("#date-popover").hide();
-            $("#date-popover").click(function (e) {
-                $(this).hide();
-            });
-        
-            $("#my-calendar").zabuto_calendar({
-                action: function () {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function () {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [
-                    {type: "text", label: "Special event", badge: "00"},
-                    {type: "block", label: "Regular event", }
-                ]
-            });
-        });
-        
-        
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-    </script>
+  ?>
   
-
   </body>
 </html>
