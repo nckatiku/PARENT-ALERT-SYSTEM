7f0ce869fc4e0474e48b73bcd292c78feea6b5b6
rajiv-shrivastava <rjv834@gmail.com>
2015-05-21 17:31:13 +0530

    Update main2.php

--- main2.php
+++ main2.php
@@ -223,66 +223,7 @@ header("Location:login_lit.php");
     {
             echo $cont;
                          
-             /* place ur api here and enjoy */
-              $mobilenumbers =$cont;
-              
-                      $message = $mes;
-            
-          $user="swap81099"; //your username
-                    $password="86235184"; //your password
-                    $senderid="SMSCountry"; //Your senderid
-                    $messagetype="N"; //Type Of Your Message
-                    $DReports="Y"; //Delivery Reports
-                    $url="http://www.smscountry.com/SMSCwebservice_Bulk.aspx";
-                    $message = urlencode($message);
-          
-          
-                            
-              $ch = curl_init();
-                            if (!$ch){die("Couldn't initialize a cURL handle");}
-                            $ret = curl_setopt($ch, CURLOPT_URL,$url);
-                            curl_setopt ($ch, CURLOPT_POST, 1);
-                            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-                            curl_setopt ($ch, CURLOPT_POSTFIELDS,
-                "User=$user&passwd=$password&mobilenumber=$mobilenumbers&message=$message&sid=$senderid&mtype=$messagetype&DR=$DReports");
-                            $ret = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                            //If you are behind proxy then please uncomment below line and provide your proxy ip with port.
-                            // $ret = curl_setopt($ch, CURLOPT_PROXY, "PROXY IP ADDRESS:PORT");
-                            $curlresponse = curl_exec($ch); // execute
-                            if(curl_errno($ch))
-                            echo 'curl error : '. curl_error($ch);
-                            if (empty($ret)) 
-          {
-                      // some kind of an error happened
-                         die(curl_error($ch));
-                         curl_close($ch); // close cURL handler
-                } 
-        else 
-        {
-                          $info = curl_getinfo($ch);
-                          curl_close($ch); // close cURL handler
-                           //echo "";
-            
-                          
-                          echo $curlresponse;
-              echo '<article class="col-md-offset-2 col-md-10"><div style="margin-top:-2%; font-size:18px; color:rgb(0,0,220)"><div class="centered">Message Sent Succesfully </div> </div> </article>' ;
-                         
-
-         } 
-           
-
-   }
-        else
-  {
- echo '<article class="col-md-offset-2 col-md-10"><div style="margin-top:-2%; font-size:15px "><div style="color:rgb(248,81,81)"><div class="centered">Warning::::Blank message or no contact numbers selected please  resend</div></div></div></article>';
-        
-  }
   
-}
-       
-
- }
 
  
 
